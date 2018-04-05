4dbc83fc457e2d81b5e8d8eb9c08186b76d8926c
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-04-05 14:02:42 +0200

    Used utility Class for Screen

---
+++ src/com/sm/net/miniviewer/util/ScreenUtil.java
@@ -0,0 +1,9 @@
+package com.sm.net.miniviewer.util;
+
+public class ScreenUtil {
+
+	public static double getRatioSize(double screenSize, double originalSize, double otherSide) {
+		double ratio = screenSize * 100 / originalSize;
+		return (otherSide * ratio / 100);
+	}
+}
--- src/com/sm/net/miniviewer/view/ViewerImage.java
+++ src/com/sm/net/miniviewer/view/ViewerImage.java
@@ -1,6 +1,7 @@
 package com.sm.net.miniviewer.view;
 
 import com.sm.net.miniviewer.model.MyFile;
+import com.sm.net.miniviewer.util.ScreenUtil;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
@@ -45,26 +46,22 @@ public class ViewerImage {
 	private void setNewSize(double width, double height, double screenWidth, double screenHeight) {
 		if (width > height) {
 			this.newWidth = screenWidth;
-			this.newHeight = getRatioSize(screenWidth, width, height);
+			this.newHeight = ScreenUtil.getRatioSize(screenWidth, width, height);
 
 		} else {
-			this.newWidth = getRatioSize(screenHeight, height, width);
+			this.newWidth = ScreenUtil.getRatioSize(screenHeight, height, width);
 			this.newHeight = screenHeight;
 		}
 	}
 
 	private void reverseSize(double screenWidth, double screenHeight) {
 		if (this.newWidth > this.newHeight) {
-			this.newWidth = getRatioSize(screenHeight, this.newHeight, this.newWidth);
+			this.newWidth = ScreenUtil.getRatioSize(screenHeight, this.newHeight, this.newWidth);
 			this.newHeight = screenHeight;
 		} else {
 			this.newWidth = screenWidth;
-			this.newHeight = getRatioSize(screenWidth, this.newWidth, this.newHeight);
+			this.newHeight = ScreenUtil.getRatioSize(screenWidth, this.newWidth, this.newHeight);
 		}
 	}
 
-	private double getRatioSize(double screenSize, double imageOriginalSize, double imageOtherSide) {
-		double ratio = screenSize * 100 / imageOriginalSize;
-		return (imageOtherSide * ratio / 100);
-	}
 }
